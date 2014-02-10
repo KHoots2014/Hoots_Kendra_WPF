bf63dc6eb5bb87d574ad220eee7b24ff4d7fbb67
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-10 16:59:27 -0600

    new Array, Add in Muppet array

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,9 +1,3 @@
-//Variables of Different types, Numbers, Booleans and strings
+// Arrays
 
-var age = 26; // Number Variable
-
-var isStudent = true; //Boolean Variable
-
-var firstName = "Kendra";
-
-var Phrase = 'I don\'t know \n You\'ll have to speak with the professor';
+Var muppetNames = ["Kermit","Miss Piggy", "Gonzo", "Rizzo"];
