8075c02aa827da3fee270ff85ba16fd4fd702db4
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-12 13:09:27 -0600

    New Expressions - PEMDAS - The Order of Operations

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,4 +1,7 @@
-// Expressions - Modulo Operators
+// Expressions - PEMDAS - The Order of Operations
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4= 80;
 
-var remainder = 32 % 10;
-console. log(remainder);
+//the average is the sum of the items divided by the number of items
