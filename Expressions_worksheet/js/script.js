f2ab402973ac5cf0e328e6d5f8ece9eb8eaf838a
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-12 15:49:17 -0600

    Problem 1- Sparky's Age in Human years changed to Dog Years

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,5 +1,13 @@
 //Kendra Hoots 2/12/14 Expressions Worksheet
 
+//Calculating Dog Years
+var dogAge = 1;
+const dogYears = 7;
+
+//multiply 1 and 7 to get Sparky's Age from human years into dog years
+var sparkyAge = dogAge * dogYears;
+//Print out the output “Sparky is X human years old which is X in dog years.
+console. log("Sparky is " + dogAge + " human years old which is " + sparkyAge + " in dog years.");
 
 
 
