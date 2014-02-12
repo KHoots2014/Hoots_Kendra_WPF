3f73c575e01cbc44e4729f7474d4c876de90daf7
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-12 14:08:13 -0600

    Making each as a string not numbers

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -3,5 +3,5 @@ var areaCode = 701;
 var firstPart = 403;
 var secPart = 4413
 
-var phoneNo = areaCode + firstPart +secPart;
+var phoneNo = String(areaCode) + String(firstPart) + String(secPart);
 console. log(phoneNo);
