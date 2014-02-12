2bfeedef78cb79e2a950ae464b7b51fda7b43e14
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-12 13:38:41 -0600

    New Expression- Concatenating Strings

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,4 +1,5 @@
-// Expressions with arrays
-var orangeBins = [234, 567, 883];
-var total = orangeBins[0] + orangeBins [1] + orangeBins [2];
-console. log(total);
+// Expressions- Concatenating Strings
+
+var firstName = "Kendra";
+var lastName = "Hoots";
+var fullName;
