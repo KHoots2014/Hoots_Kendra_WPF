99d131689e81b9bc5f09e4d08ca4c0cba13204c0
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-12 16:22:53 -0600

    Added Problem 2 - Slice of pie

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,6 +1,6 @@
 //Kendra Hoots 2/12/14 Expressions Worksheet
 
-//Calculating Dog Years
+//Problem 1 -Calculating Dog Years
 var dogAge = 1;
 const dogYears = 7;
 
@@ -9,5 +9,15 @@ var sparkyAge = dogAge * dogYears;
 //Print out the output “Sparky is X human years old which is X in dog years.
 console. log("Sparky is " + dogAge + " human years old which is " + sparkyAge + " in dog years.");
 
+//Problem 2 Slice of Pie
+// given variables and Constants
+var people = 10;
+var pizza = 3;
+const  slicePizza = 8;
 
+// number of slices per person
+// Result Variables
+var slice = ( pizza * slicePizza ) / people;
 
+//printout "Each person ate X slices of pizza at the party."
+console.log("Each person ate " + slice + " slices of pizza at the party.");
