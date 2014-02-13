d20c4d8e565173f05c38d916d6ad78a01d7de79d
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-12 21:51:01 -0600

    add alert message

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -3,4 +3,5 @@
 var width = prompt("we are calculationg the area of a rectangle. \nPlease enter the width");
 var height = prompt("Please enter the height.");
 var area = width * height;
-console. log(area);
+//console. log(area);//
+alert("the Area of your rectangle is ")
