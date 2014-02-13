bdafeeb55e90304a76ec35d59966fbadc5a0a045
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-13 14:40:55 -0600

    2nd constant

--- Expressions/Job:Industry/js/script.js
+++ Expressions/Job:Industry/js/script.js
@@ -3,3 +3,4 @@
 var  children = prompt("Hello, How many children do we have that need dippers changed?");
 var time = prompt("How long are we going to have them today?");
 const timeChange = time / 2;
+const dipChange = children * timeChange;
