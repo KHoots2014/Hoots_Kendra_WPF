bd6b843a945b9ba040294a82eb95e23671ad9f71
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-13 13:46:03 -0600

    full Wacky Expressions

--- Expressions/Wacky/js/script.js
+++ Expressions/Wacky/js/script.js
@@ -1,11 +1,19 @@
 //Wacky Expresstions - how long it will take to travel to Mars...
 
-var  speed= prompt("Hello, I am Marvin the Marshin. I am trying to get to my home planet, Mars. Can you please enter how fast my ship is going?");
+var  speed= prompt("Hello, I am Marvin the Martian. I am trying to get to my home planet, Mars. Can you please enter how fast my ship is going?");
 var  distance= prompt("How Far away is Mars?");
 //Equation needed  t=s/d
-const lightYear= 5878499810000;
-const travTime = speed / distance;
-const disLiYears = distance / lightYear; 
+const travTime = distance / speed; 
 //console. log(travTime);
 var results = "If Marvin is taveling at " + speed + "MPH and has to travel " + distance + " miles. \n Then it will take Marvin " + travTime + " hours to get to Mars.";
-alert(results)
+alert(results);
+
+const mPS = 186000;
+const sPY = 31557600;
+var mPLY = mPS * sPY;
+var disLiYears = distance / mPLY;
+const liSpeed = 186282; 
+var timeLiSpeed =disLiYears / liSpeed ;
+
+var results2 = " We know that there are " + mPLY + " miles in a light year. So we then " + distance + " / " + mPLY + " = " +  disLiYears + " light years to Mars. \n \n So it would take " + timeLiSpeed + " seconds if we where traveling at light speed(186,282 Miles per Second) to travel " + disLiYears+" light years.";
+alert (results2);
