47c642e0d9c4dda4fd5ab81d675f5b3ab0235d37
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-13 14:53:41 -0600

    last part

--- Expressions/Job:Industry/js/script.js
+++ Expressions/Job:Industry/js/script.js
@@ -5,3 +5,4 @@ var time = prompt("How long are we going to have them today?");
 const timeChange = time / 2;
 const dipChange = children * timeChange;
 var results = "If we have " + children + " children and we have them for " + time + " hours then we have to change them " + timeChange +" times today. How many dippers would we need? \n\n" + children +" children  X " + timeChange +" times = " + dipChange + " dippers.";
+alert (results);
