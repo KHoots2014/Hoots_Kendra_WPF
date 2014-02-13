78f5787f7016d011020bf70793aad0d52a7c0872
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-13 14:30:57 -0600

    revised full

--- Expressions/Wacky/js/script.js
+++ Expressions/Wacky/js/script.js
@@ -2,7 +2,7 @@
 
 var  speed= prompt("Hello, I am Marvin the Martian. I am trying to get to my home planet, Mars. Can you please enter how fast my ship is going?");
 var  distance= prompt("How Far away is Mars?");
-//Equation needed  t=s/d
+//Equation needed  t=d/s
 const travTime = distance / speed; 
 //console. log(travTime);
 var results = "If Marvin is taveling at " + speed + "MPH and has to travel " + distance + " miles. \n Then it will take Marvin " + travTime + " hours to get to Mars.";
