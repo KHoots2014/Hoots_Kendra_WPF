a1ebf85365dacbe161e78804e2e47f9904078801
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-12 21:47:38 -0600

    added the area expression

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -2,3 +2,5 @@
 
 var width = prompt("we are calculationg the area of a rectangle. \nPlease enter the width");
 var height = prompt("Please enter the height.");
+var area = width * height;
+console. log(area);
