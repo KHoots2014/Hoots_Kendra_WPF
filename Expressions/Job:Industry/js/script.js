83d847acd4337e8314dbb13c26e84f6ab570a455
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-13 14:44:20 -0600

    add part 3 result

--- Expressions/Job:Industry/js/script.js
+++ Expressions/Job:Industry/js/script.js
@@ -4,4 +4,4 @@ var  children = prompt("Hello, How many children do we have that need dippers ch
 var time = prompt("How long are we going to have them today?");
 const timeChange = time / 2;
 const dipChange = children * timeChange;
-var results = "If we have " + children + 
+var results = "If we have " + children + " children and we have them for " + 
