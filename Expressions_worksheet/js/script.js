2186833daa6c44112838138ef8d929b81b7d149c
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-12 18:39:40 -0600

    Add Problem 3 Slice of pie part 2

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -4,20 +4,30 @@
 var dogAge = 1;
 const dogYears = 7;
 
-//multiply 1 and 7 to get Sparky's Age from human years into dog years
+//Result Variables
 var sparkyAge = dogAge * dogYears;
 //Print out the output “Sparky is X human years old which is X in dog years.
 console. log("Sparky is " + dogAge + " human years old which is " + sparkyAge + " in dog years.");
 
 //Problem 2 Slice of Pie
+
 // given variables and Constants
 var people = 10;
 var pizza = 3;
-const  slicePizza = 8;
-
+const slicePizza = 8;
 // number of slices per person
 // Result Variables
 var slice = ( pizza * slicePizza ) / people;
-
 //printout "Each person ate X slices of pizza at the party."
 console.log("Each person ate " + slice + " slices of pizza at the party.");
+
+// Problem 3 -Slice of pie 2
+
+// Given Variables stay the same as above
+//result Variables
+people = 10;
+pizza = 4;
+// x= pizza * slices / people
+slice = pizza * slicePizza % people;
+//Printout "Sparky got X slices of pizza.
+console. log("Sparky got " + slice + " slices of pizza.");
