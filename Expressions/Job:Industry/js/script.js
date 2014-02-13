5dbe2dba8760a90a61db205a39290dce4c2033d5
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-13 14:38:43 -0600

    add second prompt

--- Expressions/Job:Industry/js/script.js
+++ Expressions/Job:Industry/js/script.js
@@ -1,4 +1,4 @@
 //job related Expressions
 
 var  children = prompt("Hello, How many children do we have that need dippers changed?");
-
+var time = prompt("How long are we going to have them today?");
