6bccf51b3dddbb4b99219998521c3baac8c9269d
KHoots2014 <KHoots2013@fullsail.edu>
2014-02-13 14:34:43 -0600

    add 1st prompt

---
+++ Expressions/Personal /js/script.js
@@ -0,0 +1,8 @@
+//Prompts and Alerts
+
+var width = prompt("we are calculationg the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height.");
+var area = width * height;
+//console. log(area);//
+var result = "The area of your rectangle is " + area + " sq feet";
+alert(result)
